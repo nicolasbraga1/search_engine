cf2b64bc2357e67e45178428f240e9407bf9b1cc
Nícolas <nicolasbrga01@gmail.com>
2023-07-14 16:42:40 -0300

    (Feat) Req 3 - Criando a função para converter os arquivos txt em dict

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,18 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file = txt_importer(path_file)
+    for f in instance._data:
+        if f["nome_do_arquivo"] == path_file:
+            return None
+    response = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file,
+    }
+    instance.enqueue(response)
+    print(response)
 
 
 def remove(instance):
