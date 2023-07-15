806a11e170d9d77fe11ee530eb974f07d7f5f1c5
Nícolas <nicolasbrga01@gmail.com>
2023-07-15 16:44:26 -0300

    (Feat) Req 7 - Criando função para ver se a palavra existe e suas sub funções

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,34 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+def create_response(cases, response, word, name):
+    if len(cases) > 0:
+        response.append({
+            'arquivo': name,
+            'ocorrencias': cases,
+            'palavra': word,
+        })
+    return response
 
+def find__words(lines, word, word_exists):
+    cases = []
+    for index, line in enumerate(lines, 1):
+        if line.lower().find(word.lower()) != -1:
+            if word_exists:
+                cases.append({'linha': index})
+            else:
+                cases.append({
+                    'linha': index,
+                    'conteudo': line,
+                })
+    return cases
+
+def exists_word(word, instance):
+    response = []
+    word_exists = True
+    for archive in instance._data:
+        archive_lines = archive['linhas_do_arquivo']
+        cases = find__words(archive_lines, word, word_exists)
+        archive_name = archive['nome_do_arquivo']
+        response = create_response(cases, response, word, archive_name)
+    return response
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
